ca13a9e7a911f026657b6f1e9cd7c3a8e15cf505
alejandrohsanchez <asanchez9112@gmail.com>
2021-08-28 18:10:10 -0700

    Updated more of the project
    WIll be introducing better game mechanics in the future. Basic game mechanics are implemented now. properties.py is a file that will hold functions (later)

--- Projects/TicTacToe.py
+++ Projects/TicTacToe.py
@@ -4,15 +4,61 @@
 Created on Sat Aug 28 01:32:59 2021
 @author: alejandrosanchez
 """
+
+#import properties as prop
+   
+def printBoard(origin, board):
+    for i in range(len(origin)):
+        if (i % 5 != 0):
+            print(origin[i], end = '')
+        else:
+            print()
+            print(origin[i], end = '')
+    print("\n")
     
-def printBoard(board):
     for i in range(len(board)):
         if (i % 5 != 0):
             print(board[i], end = '')
         else:
             print()
             print(board[i], end = '')
+    print()
 
-board = [' 1 ', " | ", " 2 ", " | ", " 3 ", "---", "-|-", "---", "-|-", "---", " 4 ", " | ", " 5 ", " | ", " 6 ", "---", "-|-", "---", "-|-", "---", " 7 ", " | ", " 8 ", " | ", " 9 "]
+def enterInput(player, C, board):
+    if (C == 1):
+        board[0] = player
+    elif (C == 2):
+        board[2] = player
+    elif (C == 3):
+        board[4] = player
+    elif (C == 4):
+        board[10] = player
+    elif (C == 5):
+        board[12] = player
+    elif (C == 6):
+        board[14] = player
+    elif (C == 7):
+        board[20] = player
+    elif (C == 8):
+        board[22] = player
+    elif (C == 9):
+        board[24] = player
 
-printBoard(board)
+def startGame(origin, board):
+    print('Player 1 (X) vs Player 2 (O)')
+    
+    for i in range(9):
+        if (i % 2 == 0):
+            choice = int(input('Player 1: '))
+            enterInput(' X ', choice, board)
+        else:
+            choice = int(input('Player 2: '))
+            enterInput(' O ', choice, board)
+        
+        printBoard(origin, board)
+
+origin = [' 1 ', " | ", " 2 ", " | ", " 3 ", "---", "-|-", "---", "-|-", "---", " 4 ", " | ", " 5 ", " | ", " 6 ", "---", "-|-", "---", "-|-", "---", " 7 ", " | ", " 8 ", " | ", " 9 "]
+board = ['   ', " | ", "   ", " | ", "   ", "---", "-|-", "---", "-|-", "---", "   ", " | ", "   ", " | ", "   ", "---", "-|-", "---", "-|-", "---", "   ", " | ", "   ", " | ", "   "]
+
+printBoard(origin, board)
+startGame(origin, board)
---
+++ Projects/properties.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 16:22:52 2021
+
+@author: alejandrosanchez
+"""
+
+def one():
+    print('you selected one')
+    
+def two():
+    print('you selected two')
+    
+def three():
+    print('you selected three')
+
+def four():
+    print('you selected four')
+    
+def five():
+    print('you selected five')
+    
+def six():
+    print('you selected six')
+    
+def seven():
+    print('you selected seven')
+    
+def eight():
+    print('you selected eight')
+    
+def nine():
+    print('you selected nine')
+
+options = {
+    1 : one(),
+    2 : two(),
+    3 : three(),
+    4 : four(),
+    5 : five(),
+    6 : six(),
+    7 : seven(),
+    8 : eight(),
+    9 : nine(),
+    }
