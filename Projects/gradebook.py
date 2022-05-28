358075859fd07c4f92f64dfcf80cdcbd8d52075d
alejandrohsanchez <52898001+alejandrohsanchez@users.noreply.github.com>
2022-05-27 21:24:21 -0400

    Removed some files.

--- Projects/gradebook.py
+++ Projects/gradebook.py
@@ -1,5 +1,6 @@
-correct = 'no'
-while(correct == 'no'):
+# Entering number of academic categories
+correct_weight_verified = False
+while(correct_weight_verified != True):
     number_of_classes = int(input("Enter the number of categories in your class: "))
     print()
     category_names = []
@@ -9,29 +10,53 @@ while(correct == 'no'):
         category_weights.append(float(input(f"""Enter the weight % for the "{category_names[i]}" category: """)))
         print()
 
-    print("Is this gradebook correct, yes or no?\n")
+    gradebook_verified = False
+    while (gradebook_verified != True):
+        print("Is this gradebook correct, yes or no?\n")
+        for i in range(number_of_classes):
+            print(f"""{category_names[i]}: {category_weights[i]}%""")
+        print()
+        correct = input("(yes or no): ")
+        print()
+        if (correct == 'yes'):
+            gradebook_verified = True
+            correct_weight_verified = True
+        elif (correct == 'no'):
+            gradebook_verified = True
+            print("\nRestarting...\n")
+        else:
+            print("Incorrect input...\n")
+
+# Grade entering and calculation
+grade_entered = False
+while (grade_entered != True):
+    category_grades = []
     for i in range(number_of_classes):
-        print(f"""{category_names[i]}: {category_weights[i]}%""")
-    print()
-    correct = input("(yes or no): ")
-    print()
-    if (correct == 'yes'):
-        break
-    else:
-        print("\nRestarting...\n")
+        category_grades.append(float(input(f"""Enter your grade % for {category_names[i]}: """)))
+        
 
-category_grades = []
-for i in range(number_of_classes):
-    category_grades.append(float(input(f"""Enter your grade % for {category_names[i]}: """)))
-    
+    numerator = 0
+    sum_of_weights = 0
+    final_grade = 0
 
-numerator = 0
-sum_of_weights = 0
-final_grade = 0
+    for i in range(number_of_classes):
+        numerator = numerator + (category_grades[i] * category_weights[i])
+        sum_of_weights = sum_of_weights + category_weights[i];
 
-for i in range(number_of_classes):
-    numerator = numerator + (category_grades[i] * category_weights[i])
-    sum_of_weights = sum_of_weights + category_weights[i];
+    final_grade = numerator/sum_of_weights
+    print(f"""\nYour final grade is: {round(final_grade, 2)}%\n""")
 
-final_grade = numerator/sum_of_weights
-print(f"""\nYour final grade is: {round(final_grade, 2)}%""")
+    submit_verified = False
+    while (submit_verified != True):
+        submit_answer = input("""Would you like to submit this grade to the gradebook or make grade changes?\n('submit' or 'make changes'): """)
+        if (submit_answer == 'submit'):
+            submit_verified = True
+            grade_entered = True
+            print("\nFinal grade submitted\n")
+
+        elif (submit_answer == 'make changes'):
+            submit_verified = True
+            print("\nMaking changes...\n")
+        
+        else:
+            print("Incorrect input...\n")
